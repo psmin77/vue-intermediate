12ed6e5fd0ee8af777a48d355c7cf734c036b72a
Seongmin <chr92@naver.com>
2023-03-30 15:39:56 +0900

    [es6/const-let] refactor

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -24,7 +24,7 @@ export default {
     },
   created() {
     if (localStorage.length > 0) {
-      for (var i=0; i < localStorage.length; i++) {
+      for (let i=0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
           const item = JSON.parse(localStorage.getItem(localStorage.key(i)));
           this.todoItems.push(item);
@@ -34,7 +34,7 @@ export default {
   },
   methods: {
     addOneItem(todoItem) {
-      var obj = { completed: false, item: todoItem };
+      const obj = { completed: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj); 
     },
